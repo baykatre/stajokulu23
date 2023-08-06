e37e864ef413f8fcbf86bad0ce54d672d56a94ce
Anıl Öztürk <info@anilozturk.co>
2023-08-06 13:10:16 +0300

    record sınıfı eklendi

--- src/main/java/com/stajokulu/library/book/BookDto.java
+++ src/main/java/com/stajokulu/library/book/BookDto.java
@@ -1,43 +1,4 @@
 package com.stajokulu.library.book;
 
-public class BookDto {
-
-    private String name;
-
-    private String author;
-
-    private Integer year;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public static BookDto create(String name, String author, Integer year){
-        BookDto bookDto = new BookDto();
-        bookDto.setName(name);
-        bookDto.setAuthor(author);
-        bookDto.setYear(year);
-
-        return bookDto;
-    }
+public record BookDto(String name, String author, Integer year) {
 }
--- src/main/java/com/stajokulu/library/book/BookMapper.java
+++ src/main/java/com/stajokulu/library/book/BookMapper.java
@@ -5,20 +5,14 @@ public class BookMapper {
     public static Book mapToBook(BookDto bookDto){
         Book book = new Book();
 
-        book.setName(bookDto.getName());
-        book.setAuthor(bookDto.getAuthor());
-        book.setYear(bookDto.getYear());
+        book.setName(bookDto.name());
+        book.setAuthor(bookDto.author());
+        book.setYear(bookDto.year());
 
         return book;
     }
 
     public static BookDto mapToBookDto(Book book){
-        BookDto bookDto = new BookDto();
-
-        bookDto.setName(book.getName());
-        bookDto.setAuthor(book.getAuthor());
-        bookDto.setYear(book.getYear());
-
-        return bookDto;
+        return new BookDto(book.getName(), book.getAuthor(), book.getYear());
     }
 }
